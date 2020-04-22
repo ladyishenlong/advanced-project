8f5b57143e08fea090bc2820c65a875aa60e8b64
ruanchenhao <30828481+ladyishenlong@users.noreply.github.com>
2020-04-22 09:09:30 +0800

    Initial commit

---
+++ src/main/java/com/ladyishenlong/advancedproject/AdvancedProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ladyishenlong.advancedproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvancedProjectApplication.class, args);
+    }
+
+}
